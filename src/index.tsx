fe4b7ecdda74784bdc6fe9985ec408cf366b76c7
Toan <toannv@winds.vn>
2025-09-26 02:00:58 +0700

    feat: build local library

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import LibrarySampleToannv from './NativeLibrarySampleToannv';
-import { getDeviceId } from 'react-native-device-info';
+import DeviceInfo from 'react-native-device-info';
 
 export function multiply(a: number, b: number): number {
   return LibrarySampleToannv.multiply(a, b);
 }
 
 export function getInfoDevice() {
-  const deviceId = getDeviceId();
-  console.log('Device ID: ', deviceId);
   return LibrarySampleToannv.getInfoDevice();
+}
+
+export function getDeviceId() {
+  return DeviceInfo.getUniqueId();
 }
