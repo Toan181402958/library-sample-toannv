0ea9e1288f47bf0224dcd1f1cd7070c1c9d7de24
Toan <toannv@winds.vn>
2025-09-26 01:13:29 +0700

    feat: add func get info devide

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import LibrarySampleToannv from './NativeLibrarySampleToannv';
+import { getDeviceId } from 'react-native-device-info';
 
 export function multiply(a: number, b: number): number {
   return LibrarySampleToannv.multiply(a, b);
+}
+
+export function getInfoDevice() {
+  const deviceId = getDeviceId();
+  console.log('Device ID: ', deviceId);
+  return LibrarySampleToannv.getInfoDevice();
 }
