0ab8f2c5541da12435326c5ed3993023c02c0c28
Nguyễn Danh Dũng <dungnd@techasians.com>
2025-09-26 14:37:22 +0700

    feat: config app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello from Library App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 18, fontWeight: '600' },
+});
